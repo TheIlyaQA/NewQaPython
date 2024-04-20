c641c8e999183aa7dc34aa8a1ea8ef20ecce336a
IlyaQA <ilyanechytaylo@gmail.com>
2024-04-20 17:48:29 +0300

    Updated

---
+++ homework3.py
@@ -0,0 +1,9 @@
+countries_names = ['Ukraine', 'Spain', 'Italy']
+countries_capitals = {
+    countries_names[0]:'Kyiv',
+    countries_names[1]:'Madrid',
+    countries_names[2]:'Rome'
+}
+print(f"{countries_names[0]}: {countries_capitals[countries_names[0]]}")
+print(f"{countries_names[1]}: {countries_capitals[countries_names[1]]}")
+print(f"{countries_names[2]}: {countries_capitals[countries_names[2]]}")
