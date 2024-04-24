44adaa340d07df492e99a7587433cf1d6a8cdad7
IlyaQA <ilyanechytaylo@gmail.com>
2024-04-24 22:53:50 +0300

    lesson notes

---
+++ lesson4.py
@@ -0,0 +1,40 @@
+# # string1 = "Line 1"
+# # string2 = 'Line 2'
+# # multi_line = """Line1
+# # Line2
+# # Line3
+# # """
+# # print(multi_line)
+# # print(repr(multi_line))
+# #
+# # string_name = "Metro station 'Kyivska' "
+# # print(string_name)
+# #
+# # string_name2 = 'Metro station \'Kyivska\''
+# # print(string_name2)
+# #
+# # path = "C:\\new_directory"
+# # print(path)
+# #
+# # str()
+#
+# print(len('ABC'))
+# str1 = 'aaa'
+# str2 = 'bbb'
+# str3 = str1 + str2
+# print(str3)
+#
+# string_name = "Metro station 'Kyivska' "
+# for i in string_name:
+#     print(i)
+#
+# str1 = 'ab'
+# print(str1 * 3)
+#
+# string_name = "Metro station 'Kyivska' "
+# name = 'Kyivska'
+# print(name in string_name)
+# print(name not in string_name)
+
+string_name = "Metro station 'Kyivska'"
+print(string_name[0])
